7448f76e223e8d450090795d46668da5234d1864
Rob Schoening <robschoening@gmail.com>
2015-03-08 22:38:37 -0700

    switch to mockwebserver

--- macgyver-plugin-atlassian/src/test/java/io/macgyver/plugin/atlassian/jira/JiraClientTest.java
+++ macgyver-plugin-atlassian/src/test/java/io/macgyver/plugin/atlassian/jira/JiraClientTest.java
@@ -15,71 +15,66 @@ package io.macgyver.plugin.atlassian.jira;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import io.macgyver.core.test.StandaloneServiceBuilder;
-import io.macgyver.test.MockServerTest;
+import io.macgyver.test.MacGyverIntegrationTest;
 
-import org.assertj.core.api.Assertions;
+import org.junit.Rule;
 import org.junit.Test;
-import org.mockserver.mock.Expectation;
-import org.mockserver.mock.action.ExpectationCallback;
-import org.mockserver.model.HttpCallback;
-import org.mockserver.model.HttpRequest;
-import org.mockserver.model.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hazelcast.security.Credentials;
-import com.squareup.okhttp.Authenticator;
+import com.squareup.okhttp.mockwebserver.MockResponse;
+import com.squareup.okhttp.mockwebserver.RecordedRequest;
+import com.squareup.okhttp.mockwebserver.rule.MockWebServerRule;
 
-public class JiraClientTest extends MockServerTest {
+public class JiraClientTest   {
 
 	Logger logger = LoggerFactory.getLogger(JiraClientTest.class);
 
-	public static class TestCallback implements ExpectationCallback {
-		@Override
-		public HttpResponse handle(HttpRequest httpRequest) {
+	@Rule
+	public final MockWebServerRule mockServer = new MockWebServerRule();
 
-			Assertions.assertThat(true).isFalse();
-			return HttpResponse.response("{}");
-		}
-	}
-
+	
+	/**
+	 * This is not a test of the actual JIRA REST API.  But it does test that the payload and auth headers
+	 * are intact.
+	 * @throws Exception
+	 */
 	@Test
-	public void testPostIssue() throws Exception {
+	public void testPost() throws Exception {
 
-		getMockServerClient().reset(); // clear out all expectations
-
-		// respond with a dummy response
-		getMockServerClient().when(
-				HttpRequest.request().withPath("/rest/issue")).respond(
-				HttpResponse.response("{}"));
-
+	
 		
-		
+		mockServer.enqueue(new MockResponse().setBody("{\"foo\":\"bar\"}"));
+	
+
 		// create a jira client that points to our mock server
 		JiraClient client = StandaloneServiceBuilder
 				.forServiceFactory(JiraServiceFactory.class)
-				.property("url", getMockServerUrl() + "/rest").property("username", "JerryGarcia").property("password", "Ripple")
-				.build(JiraClient.class);
+				.property("url", mockServer.getUrl("/rest").toString())
+				.property("username", "JerryGarcia")
+				.property("password", "Ripple").build(JiraClient.class);
 
 		JsonNode body = new ObjectMapper().createObjectNode().put("hello",
 				"world");
-		client.postJson("issue", body);
+		JsonNode response = client.postJson("issue", body);
 
+		assertThat(response.path("foo").asText()).isEqualTo("bar");
 		
-		// Now we can go back and make sure that what we sent matches what we think we should have sent
-		Expectation exp = getMockServerClient().retrieveAsExpectations(null)[0];
-
+		// Now we can go back and make sure that what we sent matches what we
+		// think we should have sent
 		
+		RecordedRequest recordedRequest = mockServer.takeRequest();
+		
+	
 		// Make sure we sent heaers appropriately
-		assertThat(exp.getHttpRequest().getFirstHeader("Authorization")).contains("Basic SmVycnlHYXJjaWE6UmlwcGxl");
-		assertThat(exp.getHttpRequest().getFirstHeader("Content-Type")).contains("application/json");
-		
-		// Now look at the body
-		JsonNode n = new ObjectMapper().readTree(exp.getHttpRequest()
-				.getBodyAsString());
-		assertThat(n.get("hello").asText()).isEqualTo("world");
+		assertThat(recordedRequest.getHeader("authorization"))
+				.contains("Basic SmVycnlHYXJjaWE6UmlwcGxl");  // basic auth for JerryGarcia/Ripple
+		assertThat(recordedRequest.getHeader("Content-Type"))
+				.contains("application/json");
+
+	
 
 	}
 }
